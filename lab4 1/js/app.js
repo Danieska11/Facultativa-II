847a1d3d6a8dc76681673d2a418df6f5cfe77c81
unknown <danieskalopezobando11@gmail.com>
2017-09-21 08:44:16 -0600

    guardado

---
+++ lab4 1/js/app.js
@@ -0,0 +1,17 @@
+
+
+document.getElementById('monto').addEventListener('input', function (e) { 
+   let m= e.target.value;//para capturar el numero
+   let i=m*0.02;
+   console.log(i);
+
+   document.getElementById('interes').innerHTML=(m*0.02).toFixed(2);
+   document.getElementById('rev').innerHTML=(m*0.05).toFixed(2);
+   document.getElementById('total').innerHTML=((m*0.02)+(m*0.05)).toFixed(2);
+
+   document.getElementById('interesm').innerHTML=((m*0.02)%12).toFixed(2);
+   document.getElementById('revm').innerHTML=((m*0.05)/12).toFixed(2);
+   document.getElementById('totalm').innerHTML=(((m*0.02)+(m*0.05))/12).toFixed(2);
+  
+
+});     
